ad88bb94895b1e571dff96a74f49e5889afae330
Haodong Ma <dryicetea@gmail.com>
2017-12-04 12:24:31 -0500

    cleanup

--- scripts/basic.py
+++ scripts/basic.py
@@ -5,6 +5,9 @@ env = RunEnv(visualize=True, max_obstacles = 10)
 observation = env.reset()
 for i in range(200):
     observation, reward, done, info = env.step(env.action_space.sample())
+    print('observation')
+    print(observation)
+    print('reward {0} done {1} info {2}'.format(reward, done, info))
     if done:
         env.reset()
 #        break
